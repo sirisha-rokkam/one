97fa93cca2215066e93f4d88b295b5b63964083b
SirishaRokkam <sirisharokkam123@gmail.com>
2026-02-20 16:20:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,4 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   alert('Thank you for your message! We will get back to you soon.');
   this.reset();
 });
+//my editing
